c2e056f7e625212644127d84f00774e3773ab8ed
Abhishek Gadmale <agadmale335@gmail.com>
2025-12-03 21:04:02 +0530

    Added a Contact us form and Also Added A footer with api data and icons

--- src/Components/UI/Footers.jsx
+++ src/Components/UI/Footers.jsx
@@ -1,9 +1,35 @@
+import {CiLocationOn} from "react-icons/ci";
+import {IoMdCall} from "react-icons/io";
+import {MdOutlineEmail} from "react-icons/md";
+import footerData from "../../api/footerApi.json";
+import {NavLink} from "react-router-dom";
 const Footers = () => {
+    const footer = {
+        CiLocationOn:<CiLocationOn/>,
+        IoMdCall:<IoMdCall/>,
+        MdOutlineEmail:<MdOutlineEmail/>
+    }
 return(
-    <div >
-    <h1 className="footer">Hello Footers</h1> 
-    </div>
-)
-}
+   <footer className="footer-section">
+    <div className="container grid grid-three-cols">
+{footerData.map((data,index)=>{
+    const {icon,title,details,add}= data;
+    return (
+        <div key={index} className="footer-contact">
+<div className="icon"> {footer[icon]}</div>
+<div className="footer-contact-text">
 
+<p>{title}</p>
+<NavLink to={add}>
+<p>{details}</p>
+</NavLink>
+</div>
+            </div>
+    )
+})}
+        </div>
+
+   </footer>
+)}
 export default Footers;
+
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,21 @@
 const Contact = () => {
+    const handleFormSubmit = (formData)=>{
+        const formInputData= Object.fromEntries(formData.entries())
+        console.log(formInputData);
+    }
 return(
-    <div>
-        Contact
-        contact us: 9226926752
+   <section className="section-contact">
+    <h2 className="container-title">Contact Us</h2>
+    <div className="contact-wrapper container">
+    <form action={handleFormSubmit}>
+        <input type="text" required placeholder="Enter Your Name" name="userName" autoComplete="false"/>
+        <input type="email" required placeholder="Enter Email" name="email" autoComplete="false"/>
+        <textarea name="message" rows={10} autoComplete="false" placeholder="Enter Your Message" ></textarea>
+        <button type="submit" >Submit</button>
+    </form>
+     
     </div>
+   </section>
 )
 }
 export default Contact;
